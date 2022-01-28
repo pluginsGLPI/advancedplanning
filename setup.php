7b0bd73cf48cef7a254f5a4e2a9c3adc40884c36
Cédric Anne <cedric.anne@gmail.com>
2022-01-28 16:08:48 +0100

    Fix licence headers

--- setup.php
+++ setup.php
@@ -1,29 +1,31 @@
 <?php
-/*
- -------------------------------------------------------------------------
- advancedplanning plugin for GLPI
- Copyright (C) 2019 by the advancedplanning Development Team.
 
- https://github.com/pluginsGLPI/advancedplanning
- -------------------------------------------------------------------------
-
- LICENSE
-
- This file is part of advancedplanning.
-
- advancedplanning is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- advancedplanning is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with advancedplanning. If not, see <http://www.gnu.org/licenses/>.
- --------------------------------------------------------------------------
+/**
+ * -------------------------------------------------------------------------
+ * AdvancedPlanning plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of AdvancedPlanning.
+ *
+ * AdvancedPlanning is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AdvancedPlanning is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AdvancedPlanning. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2019-2022 by AdvancedPlanning plugin team.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/pluginsGLPI/advancedplanning
+ * -------------------------------------------------------------------------
  */
 
 define('PLUGIN_ADVANCEDPLANNING_VERSION', '0.2.0');
