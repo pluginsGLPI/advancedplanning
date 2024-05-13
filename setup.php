fbabcd38f1c200d9a6b3a52abdbfccb3c83d7a4e
Rom1-B <rbiot@teclib.com>
2024-05-13 15:32:23 +0200

    phpcs + phpstan

--- setup.php
+++ setup.php
@@ -38,46 +38,49 @@ define('PLUGIN_ADVANCEDPLANNING_GLPIMAX', '10.0.99');
  *
  * @return void
  */
-function plugin_init_advancedplanning() {
-   global $PLUGIN_HOOKS, $CFG_GLPI;
+function plugin_init_advancedplanning()
+{
+    /** @var array $CFG_GLPI */
+    /** @var array $PLUGIN_HOOKS */
+    global $PLUGIN_HOOKS, $CFG_GLPI;
 
-   $PLUGIN_HOOKS['csrf_compliant']['advancedplanning'] = true;
+    $PLUGIN_HOOKS['csrf_compliant']['advancedplanning'] = true;
 
    // manage list of authorized url where to load js/css
-   $calendar_urls = [
-      "front/planning.php",
-      "front/reservation.php",
-   ];
-   foreach ($CFG_GLPI['reservation_types'] as $reservation_type) {
-      $calendar_urls[] = $reservation_type::getFormUrl(false);
-   }
+    $calendar_urls = [
+        "front/planning.php",
+        "front/reservation.php",
+    ];
+    foreach ($CFG_GLPI['reservation_types'] as $reservation_type) {
+        $calendar_urls[] = $reservation_type::getFormUrl(false);
+    }
 
-   $found_url = false;
-   foreach ($calendar_urls as $url) {
-      if (strpos($_SERVER['REQUEST_URI'] ?? '', $url) !== false) {
-         $found_url = true;
-         break;
-      }
-   }
+    $found_url = false;
+    foreach ($calendar_urls as $url) {
+        if (strpos($_SERVER['REQUEST_URI'] ?? '', $url) !== false) {
+            $found_url = true;
+            break;
+        }
+    }
 
-   if ($found_url) {
-      $sc_lib = "lib/fullcalendar-scheduler-4.4.0/packages-premium";
+    if ($found_url) {
+        $sc_lib = "lib/fullcalendar-scheduler-4.4.0/packages-premium";
 
-      $PLUGIN_HOOKS['add_javascript']['advancedplanning'] = [
-         "$sc_lib/resource-common/main.js",
-         "$sc_lib/timeline/main.js",
-         "$sc_lib/resource-timeline/main.js"
-      ];
+        $PLUGIN_HOOKS['add_javascript']['advancedplanning'] = [
+            "$sc_lib/resource-common/main.js",
+            "$sc_lib/timeline/main.js",
+            "$sc_lib/resource-timeline/main.js"
+        ];
 
-      $PLUGIN_HOOKS['add_css']['advancedplanning'] = [
-         "$sc_lib/timeline/main.css",
-         "$sc_lib/resource-timeline/main.css"
-      ];
+        $PLUGIN_HOOKS['add_css']['advancedplanning'] = [
+            "$sc_lib/timeline/main.css",
+            "$sc_lib/resource-timeline/main.css"
+        ];
 
-      $PLUGIN_HOOKS['planning_scheduler_key']['advancedplanning'] = function() {
-         return "GPL-My-Project-Is-Open-Source";
-      };
-   }
+        $PLUGIN_HOOKS['planning_scheduler_key']['advancedplanning'] = function () {
+            return "GPL-My-Project-Is-Open-Source";
+        };
+    }
 }
 
 
@@ -87,18 +90,19 @@ function plugin_init_advancedplanning() {
  *
  * @return array
  */
-function plugin_version_advancedplanning() {
-   return [
-      'name'           => 'advancedplanning',
-      'version'        => PLUGIN_ADVANCEDPLANNING_VERSION,
-      'author'         => '<a href="http://www.teclib.com">Teclib\'</a>',
-      'license'        => 'GPLV3',
-      'homepage'       => '',
-      'requirements'   => [
-         'glpi' => [
-            'min' => PLUGIN_ADVANCEDPLANNING_GLPIMIN,
-            'max' => PLUGIN_ADVANCEDPLANNING_GLPIMAX,
-         ]
-      ]
-   ];
+function plugin_version_advancedplanning()
+{
+    return [
+        'name'           => 'advancedplanning',
+        'version'        => PLUGIN_ADVANCEDPLANNING_VERSION,
+        'author'         => '<a href="http://www.teclib.com">Teclib\'</a>',
+        'license'        => 'GPLV3',
+        'homepage'       => '',
+        'requirements'   => [
+            'glpi' => [
+                'min' => PLUGIN_ADVANCEDPLANNING_GLPIMIN,
+                'max' => PLUGIN_ADVANCEDPLANNING_GLPIMAX,
+            ]
+        ]
+    ];
 }
